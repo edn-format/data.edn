3f52207f867c494fa38cf8edd32dabd9cbf8ce56
Tom Hickey <thickey@gmail.com>
2012-11-13 10:24:52 -0500

    Reworked ns-str and ns-symbol generators, included default sizers.

--- src/edn_data_gen/generators.clj
+++ src/edn_data_gen/generators.clj
@@ -2,20 +2,37 @@
   (require [clojure.test.generative.generators :as gen]
            [clojure.string :as string]))
 
+(def default-ns-partitions-sizer
+  ^{:doc "Default sizer used to determine the number of partitions a generated namespace will have."}
+  #(gen/uniform 1 6))
+
+(defn rescaled-geometric
+  "Returns a sizer with geometric distribution around n"
+  [n]
+  #(gen/geometric (/ 1 n)))
+
+(def default-ns-part-length-sizer
+  ^{:doc "Default sizer used to determine the length of symbol."}
+  (rescaled-geometric 10))
+
 (defn ns-str
+  "Returns a string to be passed into core/symbol as the namespace. Number of partitions of namespace is determined by sizer supplied in first argument, length of the parts are determined by the sizer supplied in second argument"
   ([]
-     (str (gen/symbol)))
-  ([& sizers]
-     (string/join "." (for [sizer sizers] (gen/symbol sizer)))))
+     (ns-str default-ns-partitions-sizer default-ns-part-length-sizer))
+  ([partitions-sizer part-length-sizer]
+     (let [parts (inc (partitions-sizer))]
+       (string/join "." (for [_ (range parts)]
+                          (gen/symbol part-length-sizer))))))
 
 
 (defn ns-symbol
+  "Generates a fully qualified symbol. Number of partitions determined by sizer in first argument, length of parts determined by sizer in second argument."
   ([]
-     (symbol (ns-str) (str (gen/symbol))))
-  ([& sizers]
-     (let [ns-sizers (butlast sizers)
-           sym-sizer (last sizers)]
-       (symbol (apply ns-str ns-sizers) (str (gen/symbol sym-sizer))))))
+     (ns-symbol default-ns-partitions-sizer default-ns-part-length-sizer))
+  ([ns-partitions-sizer]
+     (ns-symbol ns-partitions-sizer default-ns-part-length-sizer))
+  ([ns-partitions-sizer part-length-sizer]
+     (symbol (ns-str ns-partitions-sizer part-length-sizer) (str (gen/symbol part-length-sizer)))))
 
 (comment
 
@@ -26,7 +43,7 @@
 ;;=> my______.name____.space___.is______.super___/awesome_
 
 
-(ns-symbol 5 4 16) ;; give me (sizer 5) ns-partions of (sizer 8) length
+;; X (ns-symbol 5 4 16) ;; give me (sizer 5) ns-partions of (sizer 8) length
 ;;=> my__.name.spac.is__.supr/awesome__________________________
 
 
