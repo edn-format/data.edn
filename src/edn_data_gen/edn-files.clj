459ff47e5e8778c889c6a301b3233f81ed82cbc8
Jim Altieri <jaltieri@thortech-solutions.com>
2012-11-14 17:41:43 -0500

    file-of-numbers (mixed number types)

--- src/edn_data_gen/edn-files.clj
+++ src/edn_data_gen/edn-files.clj
@@ -1,12 +1,22 @@
 (ns edn-data-gen.edn-files
   (require [clojure.test.generative.generators :as gen]
+           [clojure.string :as string]
            [clojure.java.io :as io]
            [edn-data-gen.generators :as edngen]))
 
 (defn write-edn
-  ([data]
-     (write-edn data "output/test.txt"))
+  ([data] ;;data is a sequence of edn elements to be written
+     (write-edn data "output/out.edn"))
   ([data filename]
-     (with-open [wrtr (io/writer filename)]
-       (.write wrtr (str data)))))
+     (with-open [wrtr (io/writer (str "output/" filename))]
+       (.write wrtr (string/join " " data)))))
 
+(def num-gens
+  [gen/int
+   gen/long
+   gen/float
+   gen/double])
+
+(defn file-of-numbers
+  ([n filename]
+     (write-edn (repeatedly n (fn [] ((rand-nth num-gens)))) filename)))
