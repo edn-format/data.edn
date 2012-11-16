47477141dac3b8fbd26368c8c7641f3ce817f9ef
Tom Hickey <thickey@gmail.com>
2012-11-16 10:49:16 -0500

    Reworking file creation to properly ensure existence of the parent directory

--- src/edn_data_gen/edn-files.clj
+++ src/edn_data_gen/edn-files.clj
@@ -4,12 +4,63 @@
            [clojure.java.io :as io]
            [edn-data-gen.generators :as edngen]))
 
+(defn- file-exists?
+  "returns a bool indicating whether a dir or file exists"
+  [path]
+  (.exists (java.io.File. path)))
+
+(defn- mkdir
+  [path]
+  (.mkdir (java.io.File. path)))
+
+(defn ls
+  [path]
+  (let [file (java.io.File. path)]
+    (if (.isDirectory file)
+      (seq (.list file))
+      (when (.exists file)
+        [path]))))
+
+(defn mkdirs [path]
+  (.mkdirs (io/file path)))
+
+(defn make-parent-dirs [path]
+  (-> path
+      io/file
+      .getParent
+      mkdirs))
+
+(defn ensure-directory!
+  [path]
+  (when-not (ls path)
+    (mkdirs path)))
+
+(defn ensure-parent-directory!
+  [path]
+  (when-not (ls path)
+    (make-parent-dirs path)))
+
+(defn out-parent
+  []
+  (str (System/getProperty "user.dir")
+       java.io.File/separator
+       "output"))
+
+(defn out-path
+  [filename]
+  (str (out-parent)
+       java.io.File/separator
+       filename))
+
 (defn write-edn
-  ([data] ;;data is a sequence of edn elements to be written
-     (write-edn data "output/out.edn"))
-  ([data filename]
-     (with-open [wrtr (io/writer (str "output/" filename))]
-       (.write wrtr (string/join " " data)))))
+  "Prints out an data structure in end format to a file."
+  ([data]
+     (write-edn (out-path "out.edn") data))
+  ([filename data]
+     (binding [*print-dup* true]
+       (ensure-parent-directory! filename)
+       (spit (str filename)
+             (with-out-str (prn data))))))
 
 (def num-gens
   [gen/int
@@ -19,24 +70,24 @@
 
 (defn file-of-ints
   ([n]
-     (file-of-ints n "ints.edn"))
-  ([n filename]
-     (write-edn (repeatedly n gen/int) filename)))
+     (file-of-ints (out-path "ints.edn") n))
+  ([filename n]
+     (write-edn filename (repeatedly n gen/int))))
 
 (defn file-of-floats
   ([n]
-     (file-of-floats n "floats.edn"))
-  ([n filename]
-     (write-edn (repeatedly n gen/float) filename)))
+     (file-of-floats (out-path "floats.edn") n))
+  ([filename n]
+     (write-edn filename (repeatedly n gen/float))))
 
 (defn file-of-numbers
   ([n]
-     (file-of-numbers n "numbers.edn"))
-  ([n filename]
-     (write-edn (repeatedly n (fn [] ((rand-nth num-gens)))) filename)))
+     (file-of-numbers (out-path "numbers.edn") n))
+  ([filename n]
+     (write-edn filename (repeatedly n (fn [] ((rand-nth num-gens)))))))
 
 (defn file-of-keywords
   ([n]
-     (file-of-keywords n "keywords.edn"))
-  ([n filename]
-     (write-edn (repeatedly n gen/keyword) filename)))
+     (file-of-keywords (out-path "keywords.edn") n))
+  ([filename n]
+     (write-edn filename (repeatedly n gen/keyword))))
