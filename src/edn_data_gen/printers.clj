056c21ff6a711437fd0ad071531ba7100e8e55b2
Tom Hickey <thickey@gmail.com>
2012-11-19 16:46:16 -0500

    pr-sequential

--- src/edn_data_gen/printers.clj
+++ src/edn_data_gen/printers.clj
@@ -2,9 +2,24 @@
   "Short package description."
   (:require [edn-data-gen.protocols.print :as print]))
 
+(defn pr-sequential-writer
+  [begin separator end coll]
+  (print begin)
+  (when (seq coll)
+    (print/print-edn (first coll)))
+  (doseq [o (next coll)]
+    (print separator)
+    (print/print-edn o))
+  (print end))
 
 (extend-protocol print/IPrintEDN
+  clojure.lang.PersistentVector
+  (print-edn [this]
+    (pr-sequential-writer "["
+                          " "
+                          "]"
+                          this))
   clojure.lang.IPersistentCollection
-  (print-edn [this] (pr (str "DO IT " this)))
+  (print-edn [this] )
   java.lang.Object
   (print-edn [this] (pr this)))
