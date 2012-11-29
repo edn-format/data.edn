6e27867a377937ab1451ec0f20e67d392b5ebcd7
Tom Hickey <thickey@gmail.com>
2012-11-29 09:44:45 -0500

    Codox doc generation

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,7 @@
 {:dev
- {:dependencies [[codox-md "0.1.0"]]
-  :codox {:writer codox-md.writer/write-docs}}
+ {:plugins [[codox "0.6.3"]]
+  :codox {:src-dir-uri "http://github.com/enclojure/edn-gen/blob/refactor"
+          :src-linenum-anchor-prefix "L"}}
  :release
  {:plugins [[lein-set-version "0.2.1"]]
   :set-version
